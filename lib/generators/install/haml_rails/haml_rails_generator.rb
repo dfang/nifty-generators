6d49187716e7ab624f698914fa0bb907ea20766e
dfang <df1228@gmail.com>
2014-06-12 22:59:20 +0800

    Add simple-form and factory_girl_rails install generators and some locale files

--- lib/generators/install/haml_rails/haml_rails_generator.rb
+++ lib/generators/install/haml_rails/haml_rails_generator.rb
@@ -1,12 +1,12 @@
 module Install
   module Generators
-    class HamlGenerator < Rails::Generators::Base
+    class HamlRailsGenerator < Rails::Generators::Base
       def copy_generator_files
         spec = Gem::Specification.find_by_name("haml-rails")
         gem_root = spec.gem_dir
         generators_files = gem_root + "/lib/generators"
         FileUtils.mkdir_p 'lib/generators/haml'
-        FileUtils.cp_r generators_files, 'lib/', verbose: true
+        FileUtils.cp_r generators_files, 'lib/'
         p 'copied generators(lib/generators/*) in haml-rails to lib/generators'
       end
     end
